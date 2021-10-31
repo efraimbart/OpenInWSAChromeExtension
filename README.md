4433cac065da87cf997329398ac0805e5e51ec1d
efraimbart <efraim@rallybound.com>
2021-10-31 00:04:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Open in  WSA Chrome Extension
+Chrome extension to open links in WSA using https://github.com/efraimbart/OpenInWSA
---
+++ background.js
@@ -0,0 +1,11 @@
+﻿const openInWsa = "openInWSA";
+
+chrome.runtime.onInstalled.addListener(details =>
+    chrome.contextMenus.create({
+        title: "Open link in WSA",
+        id: openInWsa,
+        contexts: ["link"]
+    })
+);
+
+chrome.contextMenus.onClicked.addListener((info, tab) => info.menuItemId === openInWsa && chrome.tabs.update({url:`openinwsaurl://${info.linkUrl}`}));
